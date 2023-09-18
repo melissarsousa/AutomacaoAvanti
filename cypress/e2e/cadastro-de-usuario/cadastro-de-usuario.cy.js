62d9841854ac9b1d4b6a6af024c3ddf0083f9561
Melissa <melissa.rios.sousa@gmail.com>
2023-09-18 11:19:41 -0300

    Cenário 1 sucesso

--- cypress/e2e/cadastro-de-usuario/cadastro-de-usuario.cy.js
+++ cypress/e2e/cadastro-de-usuario/cadastro-de-usuario.cy.js
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Cenário Cadastro de Usuários', () => {
+  
+  beforeEach(() =>{
+    cy.visit('https://www.automationexercise.com/');
+  })
+  
+  it('Validar Cadastro de Usuário com dados válidos', () => {
+    cy.get('a[href="/login"]').contains(' Signup / Login').click();
+    cy.get('input[data-qa="signup-name"]').type('Teste');
+    cy.get('button[data-qa="signup-button"]').type('email-teste@gmail.com');
+    cy.get('button[data-qa="signup-button"]').click();
+  })
+
+  it('Validar Preenchimento Formulário de registro', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('Validar Cadastro de Usuário com dados inválidos', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+
 })
